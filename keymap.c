a6dd36bd255b1598cc401a3a7b8fd09f326a262f
minhtrannhat <minh@minhtrannhat.com>
2025-02-19 01:15:58 -0500

    feat: vertical OLED on master

--- keymap.c
+++ keymap.c
@@ -30,25 +30,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_ES
                                                               [2] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_NO, KC_F11, KC_F12, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_P0, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)};
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (!is_keyboard_master())
-        return OLED_ROTATION_180;
-    else
-        return rotation;
+    if (!is_keyboard_master()) return OLED_ROTATION_180;
+    return OLED_ROTATION_270;
 }
 
 //
-// Render left OLED display
+// Render OLED display
 //
 static void render_status(void) {
     // WPM
-    oled_write_P(PSTR("      "), false);
-    sprintf(wpm_str, "%03d", get_current_wpm());
+    oled_write_P(PSTR("\nMinh"), false);
+    oled_write_P(PSTR("\nTran\n\n"), false);
+    oled_write_P(PSTR("\n\nword\n"), false);
+    oled_write_P(PSTR("\nper\n"), false);
+    oled_write_P(PSTR("\nmin:"), false);
+    sprintf(wpm_str, "\n\n\n%03d", get_current_wpm());
     oled_write(wpm_str, false);
-    oled_write_P(PSTR("   WPM"), false);
 }
 
 //
-// Render right OLED display animation
+// Render OLED display animation
 //
 static void render_anim(void) {
     // Idle animation
@@ -132,10 +133,8 @@ static void render_anim(void) {
 //
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
-        // Left side
         render_status();
     } else {
-        // Right side
         render_anim();
     }
 
